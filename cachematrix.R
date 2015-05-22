b6f38802a4871d4605d0254c6bb455102f41e8d1
Chris Szikszoy <chris@szikszoy.com>
2015-05-21 23:51:58 -0700

    add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## CacheMatrix object and associated functions
 
-## Write a short comment describing this function
+## This function operates on the CacheMatrix object.  Functions to get and set the matrix and get and set the inverse matrix are included
 
 makeCacheMatrix <- function(x = matrix()) {
+    # set inverse to NULL by default
     inverse <- NULL
+    # function to set the matrix
     set <- function(y) {
         x <<- y
         inverse <<- NULL
     }
+    # function to get the matrix
     get <- function() x
+    # function to set the inverse of the matrix
     setInverse <- function(i) inverse <<- i
+    # function to get the inverse of the matrix
     getInverse <- function() inverse
+    # return the list of functions available to this object
     list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function returns the inverse of the CacheMatrix object
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
